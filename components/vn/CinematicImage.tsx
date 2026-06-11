13f302acaa4fb9e423906ed3b4d76d4d9bf315a2
Kelly <168733370+Ke-lly@users.noreply.github.com>
2026-06-11 14:19:58 -0300

    CinematicImage.tsx

--- components/vn/CinematicImage.tsx
+++ components/vn/CinematicImage.tsx
@@ -9,16 +9,21 @@ interface Props {
 export default function CinematicImage({ src }: Props) {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 1.03 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1.2 }}
-      viewport={{ once: true }}
+      key={src}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.8,
+        ease: "easeOut",
+      }}
       className="flex justify-center w-full"
     >
       <img
         src={src}
         alt=""
+        draggable={false}
         className="
+          block
           w-auto
           max-w-full
           h-auto
@@ -27,7 +32,6 @@ export default function CinematicImage({ src }: Props) {
           object-contain
           select-none
         "
-        draggable={false}
       />
     </motion.div>
   );
