dd7774dbced3299658ba0375cc2ea7b73df230ea
Kelly <168733370+Ke-lly@users.noreply.github.com>
2026-06-11 14:48:36 -0300

    PageTransition.tsx

--- components/layout/PageTransition.tsx
+++ components/layout/PageTransition.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
+
 export default function PageTransition({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0.98 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0.98 }}
+        transition={{ duration: 0.2 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
 }
